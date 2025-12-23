c7b0754fff239c08e4ad582c5985193903e93859
dvvrtmshra <devvratmishra20@gmail.com>
2025-12-23 18:56:39 +0530

    Manhattan Distance

---
+++ day05/manhattan_distance.py
@@ -0,0 +1,10 @@
+# Manhattan Distance (n-dimensional)
+
+point1 = list(map(float, input("Enter first point values: ").split()))
+point2 = list(map(float, input("Enter second point values: ").split()))
+
+if len(point1) != len(point2):
+    print("Points must have same dimensions")
+else:
+    distance = sum(abs(a - b) for a, b in zip(point1, point2))
+    print("Manhattan Distance:", distance)
